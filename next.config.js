1bdcfc6a17e92e06103200d66a9736aa3fcf4d1a
Raphael Pinto <raphaelcpinto@gmail.com>
2023-03-13 16:56:02 -0300

    fix: docker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  output: 'standalone',
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
